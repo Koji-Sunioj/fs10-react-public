b1050772259e906799c18c315e729c487e1a856f
Koji Inoue <koji.gabriel218@gmail.com>
2022-04-17 23:44:21 +0300

    added some more country details

--- assignment2/src/components/gotcountry.js
+++ assignment2/src/components/gotcountry.js
@@ -5,17 +5,27 @@ const GotCountry = ({ country }) => {
     return Object.entries(object).map((entry) => entry[1]);
   };
 
+  const getOfficial = (names) => {
+    const officialNames = Object.entries(names.nativeName).map(
+      (element) => element[1].official
+    );
+    const filtered = new Set(officialNames);
+    return [names.official, ...filtered];
+  };
+
   return (
     <GridColumn>
       <h2>
         {country.flag}
         {country.name.common}
       </h2>
+      <p>capital: {country.capital.join(", ")}</p>
+      <p>official names: {getOfficial(country.name).join(", ")}</p>
       <p>land area: {Intl.NumberFormat().format(country.area)}</p>
       <p>region: {country.subregion}</p>
       <p>population: {Intl.NumberFormat().format(country.population)}</p>
-      <p>capital: {country.capital.join(",")}</p>
-      <p>languages: {cleanObject(country.languages).join(",")}</p>
+
+      <p>languages: {cleanObject(country.languages).join(", ")}</p>
     </GridColumn>
   );
 };
